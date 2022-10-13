330c74ae82889b395df5c3c6298f44eff5bf7de3
CM <etchiedaniel1@gmail.com>
2022-10-13 13:10:53 -0700

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-/**
- *  * main - entry point
- *   *
- *    * Return: zero
- *     *
- *      */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
