fc7994b6b251f8e359ba072ac2d9219515511ff0
CM <etchiedaniel1@gmail.com>
2022-10-13 13:02:18 -0700

    added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Size of an char:  %d sizeof(char)")
+	printf("Size of an int: %d sizeof(int)")
+	printf("Size of an long:  %d sizeof(long)")
+	printf("Size of an long long int: %d sizeof(long long)")
+	printf("Size of an float: %d sizeof(float)")
+}
