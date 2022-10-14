2f074ba064d1548745dce050c94ea0b18acbc49c
CM <etchiedaniel1@gmail.com>
2022-10-14 15:57:47 -0700

    added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - put out
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
