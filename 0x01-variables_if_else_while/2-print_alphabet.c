02757b03ff61dc5e970991a350fb28b71bcb1107
CM <etchiedaniel1@gmail.com>
2022-10-14 15:11:27 -0700

    added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - put out
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int x;
+
+	for {x = "a"; x <= "z"; x++} {
+		putchar(x);
+	}
+	putchar("\n");
+	return (0);
+}
