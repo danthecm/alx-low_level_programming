c61565b6b83fcc640206ca579aef906fe5cbad52
CM <etchiedaniel1@gmail.com>
2022-10-14 15:51:43 -0700

    added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - put out
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%i", x);
+	}
+	printf("\n");
+	return (0);
+}
