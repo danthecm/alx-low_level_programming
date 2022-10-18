a63daa5238eaa9d8b79733430938a0aee62b9f45
CM <etchiedaniel1@gmail.com>
2022-10-18 16:57:50 -0700

    test

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_times_table - print n time table
+ * @n : number to multiply
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int multipler = 0;
+
+	if (n > 0 && n < 15)
+	{
+		for (; multipler < n; multipler++)
+		{
+			_putchar(multipler * n);
+			_putchar(',');
+		}
+	}
+}
