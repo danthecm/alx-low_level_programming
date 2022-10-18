074eb4ece5f3953a7194fce201f16c41bdee8973
CM <etchiedaniel1@gmail.com>
2022-10-18 21:51:48 +0100

    added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,8 @@
  * Return: zero
  */
 
-int main(void) {
+int main(void)
+{
 	char string[] = "_putchar";
 
 	int count = 0;
@@ -16,5 +17,5 @@ int main(void) {
 		_putchar(string[count]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
