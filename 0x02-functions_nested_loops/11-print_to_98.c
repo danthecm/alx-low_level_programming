ec0de035bc477d58b023b937593e9495ed0c5a83
CM <etchiedaniel1@gmail.com>
2022-10-18 16:34:47 -0700

    added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - print a integar
+ * @n: first number
+ * Return: zero or one
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
