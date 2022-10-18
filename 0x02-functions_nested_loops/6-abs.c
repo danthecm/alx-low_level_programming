e0824fc1ff065483222c7e2b3fd0edea11f746c0
CM <etchiedaniel1@gmail.com>
2022-10-18 15:47:25 -0700

    added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print absolute value
+ * @int - integer 
+ * Return: zero or one
+ */
+int _abs(int n)
+{
+	if(n >= 0)
+	{
+		return(n);
+	}
+	else
+	{
+		return(n * -1);
+	}
+}
