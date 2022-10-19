6c98242e26f0c6bb3cd246ec267bcdd9c982bf8e
CM <etchiedaniel1@gmail.com>
2022-10-18 17:08:45 -0700

    added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,15 +6,44 @@
  */
 void print_times_table(int n)
 {
-	int multipler = 0;
+	int x, y, z;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 14)
 	{
-		for (; multipler < n; multipler++)
+		for (x = 0; x <= n; x++)
 		{
-			//int result = multipler * n;
-			_putchar(0 * 1);
-			_putchar(',');
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
 		}
 	}
 }
