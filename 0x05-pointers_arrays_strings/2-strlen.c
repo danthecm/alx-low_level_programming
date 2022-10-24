1980263b01a7c847c56e35f3e8237242466b7d2e
CM <etchiedaniel1@gmail.com>
2022-10-24 23:05:10 +0100

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
-* swap_int - reset a pointer to 98
-* @s: a pointer
-* Return: void
-*/
+ * _strlen - reset a pointer to 98
+ * @s: a pointer
+ * Return: void
+ */
 
 int _strlen(char *s)
 {
-	char string = *s;
-	return (16);
+	int length = 0;
+
+while (s[length] != '\0')
+{
+	length++;
+}
+return (length);
 }
