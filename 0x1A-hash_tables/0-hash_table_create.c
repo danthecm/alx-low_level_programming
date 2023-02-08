e56f78b4e361a78ac15cc77fe651f1924f24d6cd
danthecm <etchiedaniel1@gmail.com>
2023-02-08 20:09:36 +0100

    6: 0x1A task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t) * hash_table->size);
 	if (hash_table->array == NULL)
 	{
 		return (NULL);
